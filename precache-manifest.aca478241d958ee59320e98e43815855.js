37c3287c99f261cbdae4b725d7e609dd2ebd3af6
Olimpio Kaio <olimpiocode@gmail.com>
2020-04-21 23:35:07 -0300

    Updates

---
+++ precache-manifest.aca478241d958ee59320e98e43815855.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b75e61c3360ef46bb275334c31f2dcb1",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "e3842d5743aafd82ab7e",
+    "url": "/monsters-rolodex/static/css/main.5ef4fdb0.chunk.css"
+  },
+  {
+    "revision": "147f1327e7798733efc4",
+    "url": "/monsters-rolodex/static/js/2.d562cfe4.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/monsters-rolodex/static/js/2.d562cfe4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e3842d5743aafd82ab7e",
+    "url": "/monsters-rolodex/static/js/main.7932500d.chunk.js"
+  },
+  {
+    "revision": "83fef09b6b4b91e8e383",
+    "url": "/monsters-rolodex/static/js/runtime-main.6036d2a0.js"
+  }
+]);
